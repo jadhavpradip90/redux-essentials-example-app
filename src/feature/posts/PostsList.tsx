73864f2b4993112714f68f55ef5124ad1a39e250
Pradip Jadhav <jadhavpradip90@gmail.com>
2025-07-01 07:35:50 +0530

    PostsList change

--- src/feature/posts/PostsList.tsx
+++ src/feature/posts/PostsList.tsx
@@ -4,13 +4,35 @@ import { useAppSelector, useAppDispatch } from '@/app/hooks'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from '@/components/TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
-import { fetchPosts, selectAllPosts, selectPostsStatus } from './postsSlice'
+import { fetchPosts, selectAllPosts, selectPostsStatus, selectPostsError, Post } from './postsSlice'
+import { Spinner } from '@/components/Spinner'
+
+interface PostExcerptProps {
+  post: Post
+}
+
+function PostExcerpt({ post }: PostExcerptProps) {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>
+        <Link to={`/posts/${post.id}`}>{post.title}</Link>
+      </h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <ReactionButtons post={post} />
+    </article>
+  )
+}
 
 export const PostsList = () => {
   // Select the `state.posts` value from the store into the component
   const dispatch = useAppDispatch()
   const posts = useAppSelector(selectAllPosts)
   const postStatus = useAppSelector(selectPostsStatus)
+  const postsError = useAppSelector(selectPostsError)
 
   useEffect(() => {
     if (postStatus === 'idle') {
@@ -18,25 +40,27 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch])
 
-  // Sort posts in reverse chronological order by datetime string
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  let content: React.ReactNode
 
-  const renderedPosts = orderedPosts.map(post => (
-    <article className="post-excerpt" key={post.id}>
-      <PostAuthor userId={post.user} />
-      <h3> 
-        <Link to={`/posts/${post.id}`}>{post.title}</Link>
-      </h3>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <TimeAgo timestamp={post.date}/>
-      <ReactionButtons post={post} />
-    </article>
-  ))
+  if (postStatus === 'pending') {
+    content = <Spinner text="Loading..." />
+  } else if (postStatus === 'succeeded') {
+    // Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map(post => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{postsError}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
